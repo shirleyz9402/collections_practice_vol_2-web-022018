3ed446b75e7de1ea1b1f68da85765ba0c37b32c5
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-31 21:05:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|element| element.start_with?("r")}
+end
+
+def contain_a(array)
+  array.find_all {|element| element.include? "a"}
+end 
+
+def first_wa(array)
+  array.find {|element| element.to_s.start_with?("wa")}
+end 
+
+def remove_non_strings(array)
+  array.delete_if do |element|
+    element.class != String 
+  end 
+end 
+
+def count_elements()
