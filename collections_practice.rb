5d90017d953f34bd919bf7e843e31f14ec565bd0
shirleyz9402 <shirleyz9402@yahoo.com>
2018-08-05 16:09:21 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,46 +1,54 @@
 def begins_with_r(array)
-  array.all? {|element| element.start_with?("r")}
+  array.all? do |el|
+    el.start_with?("r")
+  end
 end
 
 def contain_a(array)
-  array.find_all {|element| element.include? "a"}
-end 
+  array.find_all do |el|
+    el.include?("a")
+  end
+end
 
 def first_wa(array)
-  array.find {|element| element.to_s.start_with?("wa")}
-end 
+  array.find do |el|
+    el.to_s.start_with?("wa")
+  end
+end
 
 def remove_non_strings(array)
-  array.delete_if do |element|
-    element.class != String 
-  end 
-end 
+  array.delete_if do |el|
+    el.class != String
+  end
+end
 
 def count_elements(array)
-names = array.uniq
+  names = array.uniq
   names.map do |name|
-    name.merge(:count => array.count { |element| element[ :name] == name[:name]})
-  end 
-end 
+    name.merge(:count => array.count{|el| el[:name] == name[:name]})
+  end
+end
 
-def merge_data(keys,data)
+def merge_data(keys, data)
   keys.map do |name|
     name.merge(data[0][name[:first_name]].to_h)
-  end 
-end 
+  end
+end
 
 def find_cool(array)
-  array.find_all{ |element| element[:temperature] == "cool"}
-end 
+  array.find_all do |el|
+    el[:temperature] == "cool"
+  end
+end
 
 def organize_schools(schools)
-hash = {}
-schools.each do |school,location|
-  if hash.include?(location[:location]) == false
-    hash[location[:location]] = [school]
-  else
-    hash[location[:location]] << school
-  end 
+  hash = {}
+  schools.each do |school,location|
+    if hash.include?(location[:location])
+      hash[location[:location]] << school
+    else
+      hash[location[:location]] = [school]
+    end
+  end
+  hash
 end
-hash
-end 
